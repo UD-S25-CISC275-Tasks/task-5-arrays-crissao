3ad1f022ea35aca5b63d706ec1a64854975c04ab
Crissa Owens <159073989+crissao@users.noreply.github.com>
2025-02-21 13:30:07 -0500

    countShortWords done

--- src/arrays.ts
+++ src/arrays.ts
@@ -68,7 +68,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const shorties: string[] = words.filter((word: string): boolean => word.length < 4);
+    return shorties.length;
 }
 
 /**
